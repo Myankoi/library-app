74e275a469dce2ce259bbd78053a2c1eea2d4e49
Myan <146714203+Myankoi@users.noreply.github.com>
2024-09-10 23:34:35 +0700

    refactor (converthelper): add converthelper class

---
+++ !Services/ConvertHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library_app._Services
+{
+    public class ConvertHelper
+    {
+        private Image image;
+        public Image ConvertBase64ToImage(string imageString)
+        {
+            byte[] bytes = Convert.FromBase64String(imageString);
+
+            Image image;
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                image = Image.FromStream(ms);
+            }
+
+            return image;
+        }
+        public String ConvertImageToBase64(Image image)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (Image clonedImage = new Bitmap(image))
+                {
+                    clonedImage.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    byte[] imageBytes = ms.ToArray();
+                    return Convert.ToBase64String(imageBytes);
+                }
+            }
+        }
+    }
+}
